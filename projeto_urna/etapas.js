b716630cd9a825823e1125e3366cb0f2986f96b3
Nickolas <68036949+nickolaslr@users.noreply.github.com>
2023-02-02 08:46:36 -0300

    Add files via upload

---
+++ projeto_urna/etapas.js
@@ -0,0 +1,65 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Vereador'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Vereador'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
